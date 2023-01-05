c7c244a67060fc5037d7e2a53276fc8fb7dc282f
himanshu9999git <hchimanshu9999@gmail.com>
2023-01-06 01:31:08 +0530

    Options Added

--- src/Components/MainScreen.js
+++ src/Components/MainScreen.js
@@ -1,13 +1,52 @@
-import React, { useContext } from 'react';
-import { QuizContext } from '../Helpers/Contexts';
+import React, { useContext, useState } from "react";
+import { QuizContext } from "../Helpers/Contexts";
+import { Ques } from "../Helpers/QuestionBank";
 
-const MainScreen = () =>
-{
-    
-    const {gameState, setGameState} = useContext(QuizContext);
-    return<>
-        Main Screen
+const MainScreen = () => {
+  const { gameState, setGameState } = useContext(QuizContext);
+  const [optionChosen, setOptionChosen] = useState("");
+
+  const [currQues, setCurrQues] = useState(0);
+  return (
+    <>
+      <div className="str">
+        <h1>{Ques[currQues].prompt}</h1>
+        <div className="options">
+          <button
+            onClick={() => {
+              setOptionChosen("A");
+            }}
+          >
+            {Ques[currQues].optionA}
+          </button>
+          <button
+            onClick={() => {
+              setOptionChosen("B");
+            }}
+          >
+            {" "}
+            {Ques[currQues].optionB}
+          </button>
+          <button
+            onClick={() => {
+              setOptionChosen("C");
+            }}
+          >
+            {" "}
+            {Ques[currQues].optionC}
+          </button>
+          <button
+            onClick={() => {
+              setOptionChosen("D");
+            }}
+          >
+            {" "}
+            {Ques[currQues].optionD}
+          </button>
+        </div>
+      </div>
     </>
-}
+  );
+};
 
 export default MainScreen;
